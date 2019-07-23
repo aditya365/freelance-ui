70523be86ca803daf67ae469edc9eb09b822ff5d
praveensms <prvnsms@gmail.com>
2019-07-23 21:53:36 +0530

    Added Filters UI

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSnackBarModule, MatSelectModule, MatAutocompleteModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSnackBarModule, MatSelectModule, MatAutocompleteModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatSliderModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
@@ -16,7 +16,9 @@ import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, 
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    MatSliderModule
   ], exports:[
     MatToolbarModule,
     MatButtonModule,
@@ -29,7 +31,9 @@ import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, 
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    MatSliderModule
   ]
 })
 export class MaterialModule { }
